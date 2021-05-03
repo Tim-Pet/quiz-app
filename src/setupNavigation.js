47df324ebe4f87a6dfa6e060254acc9cd0f57794
Tim Petersen <tim.petersen1@outlook.de>
2021-05-03 16:45:31 +0200

    Refactor Functions

--- src/setupNavigation.js
+++ src/setupNavigation.js
@@ -1,23 +1,39 @@
 import getAllElements from './utils/getAllElements'
-// import getElement from './utils/getElement'
+import getElement from './utils/getElement'
 
 export default function setupNavigation() {
   const pages = getAllElements('[data-page]')
   const buttons = getAllElements('[data-nav]')
+  const header = getElement('[data-header]')
 
-  buttons.forEach(button => {
+  buttons.forEach(setupButton)
+
+  function setupButton(button) {
+    const buttonName = button.dataset.nav
+    const headerText = button.dataset.headerText
+
     button.addEventListener('click', () => {
-      const buttonName = button.dataset.nav
-
-      buttons.forEach(buttonTwo => {
-        const buttonTwoName = buttonTwo.dataset.nav
-        buttonTwo.classList.toggle('active', buttonTwoName === buttonName)
-      })
-
-      pages.forEach(page => {
-        const pageName = page.dataset.page
-        page.classList.toggle('hidden', pageName !== buttonName)
-      })
+      setHeaderName(headerText)
+      setActiveButton(buttonName)
+      setActivePage(buttonName)
     })
-  })
+  }
+
+  function setHeaderName(headerText) {
+    header.innerText = headerText
+  }
+
+  function setActiveButton(buttonName) {
+    buttons.forEach(button => {
+      const currentButtonName = button.dataset.nav
+      button.classList.toggle('active', currentButtonName === buttonName)
+    })
+  }
+
+  function setActivePage(buttonName) {
+    pages.forEach(page => {
+      const pageName = page.dataset.page
+      page.classList.toggle('hidden', pageName !== buttonName)
+    })
+  }
 }
